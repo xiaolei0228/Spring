52bab13ab77331a0abd696d99c663b901331a1e4
xiaolei <xiaolei-0228@163.com>
2015-01-04 22:42:10 +0800

    Spring_AOP(Annotation)

--- Spring_AOP(Annotation)/src/test/java/com/ehome/spring_aop/annotation/test/controller/BaseControllerTest.java
+++ Spring_AOP(Annotation)/src/test/java/com/ehome/spring_aop/annotation/test/controller/BaseControllerTest.java
@@ -16,7 +16,7 @@ import org.junit.Before;
  * @Copyright: 2014 ihome.com
  */
 public class BaseControllerTest {
-    protected String url = "http://127.0.0.1:8080/springaop/api";
+    protected String url = "http://127.0.0.1/springaop/api";
     protected HttpResponse response = null;
     protected JSONObject paramJson;
 
