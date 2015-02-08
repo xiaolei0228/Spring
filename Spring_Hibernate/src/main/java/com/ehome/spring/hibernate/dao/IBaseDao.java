9a36f99dc6b459bb3a74cec66767e6902e3490f7
haoxiaolei <xiaolei-0228@163.com>
2015-02-08 22:59:52 +0800

    Spring_Hibernate

--- Spring_Hibernate/src/main/java/com/ehome/spring/hibernate/dao/IBaseDao.java
+++ Spring_Hibernate/src/main/java/com/ehome/spring/hibernate/dao/IBaseDao.java
@@ -1,55 +1,15 @@
 package com.ehome.spring.hibernate.dao;
 
 import java.io.Serializable;
+import java.util.List;
 
 /**
  * Created by haoxiaolei on 2015-01-27 16:35
  */
 public abstract interface IBaseDao<T, PK extends Serializable> {
-    /**
-     * 保存
-     *
-     * @param entity 对象实体
-     *
-     * @return 保存后的对象
-     */
-    T save(T entity);
+    public T save(T entity);
 
-    /**
-     * 批量保存
-     *
-     * @param entities 对象集合
-     *
-     * @return 保存后的对象集合
-     */
-    Iterable<T> save(Iterable<T> entities);
+    public T get(Class<T> c, PK pk);
 
-    /**
-     * 更新
-     *
-     * @param enitty 对象实体
-     *
-     * @return 更新后的对象
-     */
-    T update(T enitty);
-
-    /**
-     * 删除
-     *
-     * @param entity 要删除的对象
-     *
-     * @return 返回删除的对象
-     */
-    T delete(T entity);
-
-    /**
-     * 根据主键起查询对象
-     *
-     * @param entityClass 对象的class
-     * @param pk          主键
-     *
-     * @return 对象实体
-     */
-    T findById(Class<T> entityClass, PK pk);
-
+    public List<T> getAllList(Class<T> c);
 }
--- Spring_Hibernate/src/main/java/com/ehome/spring/hibernate/dao/impl/BaseDaoImpl.java
+++ Spring_Hibernate/src/main/java/com/ehome/spring/hibernate/dao/impl/BaseDaoImpl.java
@@ -1,78 +1,35 @@
 package com.ehome.spring.hibernate.dao.impl;
 
 import com.ehome.spring.hibernate.dao.IBaseDao;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.orm.hibernate4.support.HibernateDaoSupport;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
+import javax.annotation.Resource;
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by haoxiaolei on 2015-01-27 16:37
  */
 @Repository("baseDao")
-public class BaseDaoImpl<T, PK extends Serializable> implements IBaseDao<T, PK> {
+public  class BaseDaoImpl<T, PK extends Serializable> extends HibernateDaoSupport implements IBaseDao<T, PK> {
 
-    @PersistenceContext
-    private EntityManager em;
+    @Resource
+    protected SessionFactory sessionFactory;
+    protected Session session = sessionFactory.getCurrentSession();
+
 
     public T save(T entity) {
-        em.persist(entity);
-        return entity;
+        return null;
     }
 
-    /**
-     * 批量保存
-     *
-     * @param entities 对象集合
-     *
-     * @return 保存后的对象集合
-     */
-    public Iterable<T> save(Iterable<T> entities) {
-        List<T> result = new ArrayList<>();
-        if (entities == null) {
-            return result;
-        }
-        for (T entity : entities) {
-            result.add(save(entity));
-        }
-        return result;
+    public T get(Class<T> c, PK pk) {
+        return (T) session.get(c, pk);
     }
 
-    /**
-     * 更新
-     *
-     * @param enitty 对象实体
-     *
-     * @return 更新后的对象
-     */
-    public T update(T enitty) {
-        return em.merge(enitty);
-    }
-
-    /**
-     * 删除
-     *
-     * @param entity 要删除的对象
-     *
-     * @return 返回删除的对象
-     */
-    public T delete(T entity) {
-        em.remove(em.merge(entity));
-        return entity;
-    }
-
-    /**
-     * 根据主键查询对象
-     *
-     * @param entityClass 对象的class
-     * @param pk          主键
-     *
-     * @return 对象实体
-     */
-    public T findById(Class<T> entityClass, PK pk) {
-        return em.find(entityClass, pk);
+    public List<T> getAllList(Class<T> c) {
+        return null;
     }
 }
