b20134b5a66cb760404403c53c018c3c2c770209
haoxiaolei <xiaolei-0228@163.com>
2015-02-13 11:14:41 +0800

    Spring_MyBatis

---
+++ Spring_MyBatis/src/main/java/com/ehome/spring/mybatis/module/User.java
@@ -0,0 +1,45 @@
+package com.ehome.spring.mybatis.module;
+
+/**
+ * Created by haoxiaolei on 2015-02-13 11:09
+ */
+public class User {
+
+    private int id;
+    private String userName;
+    private String userAge;
+    private String userAddress;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserAge() {
+        return userAge;
+    }
+
+    public void setUserAge(String userAge) {
+        this.userAge = userAge;
+    }
+
+    public String getUserAddress() {
+        return userAddress;
+    }
+
+    public void setUserAddress(String userAddress) {
+        this.userAddress = userAddress;
+    }
+}
