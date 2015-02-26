c8df1b67905e4073344bfdd736345bd57a44b570
haoxiaolei <xiaolei-0228@163.com>
2015-02-26 19:02:14 +0800

    Spring_MyBatis

--- Spring_MyBatis/src/main/java/com/ehome/spring/mybatis/dao/impl/BaseDaoImpl.java
+++ Spring_MyBatis/src/main/java/com/ehome/spring/mybatis/dao/impl/BaseDaoImpl.java
@@ -1,9 +1,7 @@
 package com.ehome.spring.mybatis.dao.impl;
 
 import com.ehome.spring.mybatis.dao.IBaseDao;
-import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.support.SqlSessionDaoSupport;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.io.Serializable;
@@ -16,10 +14,5 @@ import java.io.Serializable;
 @Repository("baseDao")
 public  class BaseDaoImpl<T, PK extends Serializable> extends SqlSessionDaoSupport implements IBaseDao<T, PK> {
 
-    private SqlSessionTemplate sessionTemplate;
 
-    @Autowired
-    public void setSessionTemplate(SqlSessionTemplate sessionTemplate) {
-        this.sessionTemplate = sessionTemplate;
-    }
 }
