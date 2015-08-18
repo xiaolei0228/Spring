57f642d79052dc229e6108cd1df113c9ec820527
haoxiaolei <xiaolei-0228@163.com>
2015-08-18 17:42:51 +0800

    Spring_WebSocket

--- Spring_WebSocket/src/main/java/com/ehome/spring/websocket/interceptor/WebSocketHandshakeInterceptor.java
+++ Spring_WebSocket/src/main/java/com/ehome/spring/websocket/interceptor/WebSocketHandshakeInterceptor.java
@@ -17,9 +17,7 @@ import java.util.Map;
 public class WebSocketHandshakeInterceptor extends HttpSessionHandshakeInterceptor {
 
     /**
-     * 在握手之前要做的事情，在这里可以与数据库交互，取得能标识当前socket是唯一的即可。<br/>
-     * 如获取当前session中的用户名，用户Id 等等，把下面的sessionId替换成这个唯一标识
-     * (目前的是用sessionId来模拟的)
+     * 在握手之前要做的事情
      * @param req
      * @param resp
      * @param wsHandler
@@ -30,6 +28,13 @@ public class WebSocketHandshakeInterceptor extends HttpSessionHandshakeIntercept
         return super.beforeHandshake(req, resp, wsHandler, attributes);
     }
 
+    /**
+     * 握手之后要做的事情
+     * @param request
+     * @param response
+     * @param wsHandler
+     * @param ex
+     */
     public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Exception ex) {
         System.out.println("After Handshake...");
         super.afterHandshake(request, response, wsHandler, ex);
